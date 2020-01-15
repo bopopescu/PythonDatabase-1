0237963a076d03655546140b2eb764e2d72bffc5
StanleyLiangYork <stanleyliang5@gmail.com>
2020-01-15 03:55:56 -0500

    Add a print

--- page_spider.py
+++ page_spider.py
@@ -5,6 +5,7 @@ import mysql.connector
 def main(database: str, url_list_file: str):
     print("open database: {}".format(database))
     print("web page will be scanned: {}".format(url_list_file))
+    print("waiting for result")
 
 
 if __name__ == "__main__":
